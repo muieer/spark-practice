951939341f0ccac8fa5fe663bbde032d6d50425f
muieer <yang_for_work@qq.com>
2023-12-19 10:25:55 +0800

    add ReadParquetFileExample

---
+++ notes/DataFrame.md
@@ -0,0 +1,7 @@
+# DataFrame 笔记
+
+## 类型转化
+[cast-string-type-to-integer-type](https://sparkbyexamples.com/spark/spark-cast-string-type-to-integer-type-int/)<br>
+
+## 添加新列
+[add-new-column-to-dataframe](https://sparkbyexamples.com/spark/spark-add-new-column-to-dataframe/)
---
+++ src/main/scala/org/muieer/scala/input/ReadParquetFileExample.scala
@@ -0,0 +1,38 @@
+package org.muieer.scala.input
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, expr, upper}
+import org.muieer.scala.buildLocalSparkEnv
+
+object ReadParquetFileExample {
+
+  var spark: SparkSession = _
+
+  def main(args: Array[String]): Unit = {
+
+    this.spark = buildLocalSparkEnv()._1
+    run(args)
+  }
+
+  def run(args: Array[String]): Unit = {
+
+    val dataFrame = spark.read.parquet(args(0))
+    dataFrame.printSchema()
+    dataFrame.take(10).foreach(println(_))
+
+    // 在原表基础上添加新列
+    val dataFrame1 = dataFrame.withColumn("feature_num_sub_100", col("feature_num") - 100)
+    dataFrame1.printSchema()
+    dataFrame1.take(10).foreach(println(_))
+
+    // 在原表基础上添加新列
+    val dataFrame2 = dataFrame.select(
+      col("*"),
+      col("feature_num").-(100).as("feature_num_sub_100")
+    )
+    dataFrame2.printSchema()
+    dataFrame2.take(10).foreach(println(_))
+
+  }
+
+}
